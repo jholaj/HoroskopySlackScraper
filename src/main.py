51613b4297b1e93b1f85e5c055226e6ca048efcc
Jaroslav Holaj <jaroslav.holaj@carebot.com>
2024-07-09 01:16:49 +0200

    feat: integrating slack functionality

--- src/main.py
+++ src/main.py
@@ -1,5 +1,14 @@
+import os
+import time
 import requests
 from bs4 import BeautifulSoup
+from slack_bolt import App
+from slack_bolt.adapter.socket_mode import SocketModeHandler
+from slack_sdk.errors import SlackApiError
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 # Base URL of the website
 BASE_URL = "https://www.horoskopy.cz"
@@ -11,6 +20,14 @@ ZODIACS = [
 ]
 
 PERCENTAGES = ["100%", "80%", "60%", "40%", "20%", "0%", "-20%", "-40%", "-60%", "-80%", "-100%"]
+
+# Load tokens from environment variables for security
+SLACK_BOT_TOKEN = os.getenv('SLACK_BOT_TOKEN')  # xoxb- token
+SLACK_APP_TOKEN = os.getenv('SLACK_APP_TOKEN')  # xapp- token
+SLACK_CHANNEL_ID = os.getenv('SLACK_CHANNEL_ID')  # slack channel ID
+
+# Initialize a Bolt for Python app with the bot token
+app = App(token=SLACK_BOT_TOKEN)
 
 
 def fetch_zodiac_data(base_url, zodiac_signs):
@@ -56,8 +73,43 @@ def format_compatibility_data(horoscope_data, percentages):
     return formatted_data
 
 
-if __name__ == '__main__':
+def send_to_slack(channel_id, message):
+    """
+    Sends a formatted message to a Slack channel.
+
+    Parameters:
+    - channel_id (str): The ID of the Slack channel where the message will be sent.
+    - message (str): The message to send to Slack.
+    """
+    try:
+        response = app.client.chat_postMessage(channel=channel_id, text=message)
+        print(f"Message sent to {channel_id}: {response['message']['text']}")
+    except SlackApiError as e:
+        print(f"Error sending message to Slack: {e.response['error']}")
+
+
+def send_daily_horoscope():
+    """
+    Fetches and sends the daily horoscope data to the Slack channel.
+    """
     raw_compatibility_data = fetch_zodiac_data(BASE_URL, ZODIACS)
     formatted_compatibility_data = format_compatibility_data(raw_compatibility_data, PERCENTAGES)
+
+    intro_message = f"*VZTAH ZNAMENÍ K OSTATNÍM ZNAMENÍ KE DNI: _{time.strftime('%d.%m.%Y').upper()}_*\n\n"
+
+    # Prepare the message to send
+    message = intro_message
+
     for sign, data in formatted_compatibility_data.items():
-        print(f"{sign.capitalize()}: {data}")
+        message += f"\n*_>{sign.capitalize().upper()}_*\n"
+        message += "\n".join(data) + "\n\n"
+
+    # Send the complete message to Slack
+    send_to_slack(SLACK_CHANNEL_ID, message)
+
+if __name__ == "__main__":
+    send_daily_horoscope()
+
+    # Run the Slack bot using Socket Mode
+    handler = SocketModeHandler(app, SLACK_APP_TOKEN)
+    handler.connect()
