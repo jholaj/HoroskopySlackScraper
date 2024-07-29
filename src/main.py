02ffcf368b56b5e1665850ce96544b79190ac25a
Jaroslav Holaj <jaroslav.holaj@carebot.com>
2024-07-29 22:33:11 +0200

    feat: Changed format of table from txt to png & minor fixes

--- src/main.py
+++ src/main.py
@@ -10,14 +10,14 @@ from slack_sdk.errors import SlackApiError
 from slack_sdk import WebClient
 from dotenv import load_dotenv
 import pandas as pd
-from tabulate import tabulate
+import imgkit
 
 load_dotenv()
 
-# Base URL of the website
 BASE_URL = "https://www.horoskopy.cz"
 
-# List of zodiac signs and their corresponding URLs
+IMG_PATH = '/tmp/horoscope_table.png'
+
 ZODIACS = [
     "beran", "lev", "strelec", "byk", "panna", "kozoroh",
     "blizenci", "vahy", "vodnar", "rak", "stir", "ryby"
@@ -25,32 +25,30 @@ ZODIACS = [
 
 PERCENTAGES = ["100%", "80%", "60%", "40%", "20%", "0%", "-20%", "-40%", "-60%", "-80%", "-100%"]
 
-# Load tokens from environment variables for security
-SLACK_BOT_TOKEN = os.getenv('SLACK_BOT_TOKEN')  # xoxb- token
-SLACK_APP_TOKEN = os.getenv('SLACK_APP_TOKEN')  # xapp- token
-SLACK_CHANNEL_ID = os.getenv('SLACK_CHANNEL_ID')  # slack channel ID
+SLACK_BOT_TOKEN = os.getenv('SLACK_BOT_TOKEN')
+SLACK_APP_TOKEN = os.getenv('SLACK_APP_TOKEN')
+SLACK_CHANNEL_ID = os.getenv('SLACK_CHANNEL_ID')
 
-# Initialize a Bolt for Python app with the bot token
 app = App(token=SLACK_BOT_TOKEN)
 client = WebClient(token=SLACK_BOT_TOKEN)
 
 
-# Load zodiac names from a JSON file
 def load_names_zodiacs(filename):
-    with open(filename, 'r') as file:
+    """
+    Load zodiac names from a JSON file.
+    """
+    with open(filename, 'r', encoding="utf-8") as file:
         return json.load(file)
 
 
-# Names and their corresponding zodiac signs
 NAMES_ZODIACS = load_names_zodiacs('../names_zodiacs.json')
 
 
 def fetch_zodiac_data(base_url, zodiac_signs):
     """
-    Fetches compatibility data for each zodiac sign from the given base URL.
+    Fetch zodiac compatibility data from the given URL.
     """
     horoscope_data = {}
-
     for sign in zodiac_signs:
         url = f"{base_url}/{sign}"
         response = requests.get(url, timeout=5)
@@ -69,89 +67,175 @@ def fetch_zodiac_data(base_url, zodiac_signs):
 
 def format_compatibility_data(horoscope_data, percentages, names_zodiacs):
     """
-    Formats the horoscope compatibility data into a DataFrame.
+    Format zodiac compatibility data into a DataFrame.
     """
-    # Create a dictionary to hold the DataFrame data
     df_data = {"Percent": percentages}
-    
+
     for sign, values in horoscope_data.items():
         formatted_values = []
         for value in values:
-            # Split the value by commas to handle multiple signs
             parts = value.split(',')
             names = []
             for part in parts:
                 part = part.strip().lower()
                 if part in names_zodiacs and names_zodiacs[part]:
-                    names.append(",".join(names_zodiacs[part]))
-            formatted_values.append(",".join(names))
+                    names.append(", ".join(names_zodiacs[part]))
+            formatted_values.append(", ".join(names))
         df_data[sign.capitalize()] = formatted_values
 
-    # Create DataFrame
     df = pd.DataFrame(df_data)
     df.set_index("Percent", inplace=True)
 
     return df
 
-def split_dataframe(df):
+
+def dataframe_to_html(df):
     """
-    Splits the DataFrame into two parts for better readability in Slack.
+    Converts a DataFrame to an HTML table using custom HTML and CSS styling.
     """
-    num_columns = len(df.columns)
-    split_point = num_columns // 2
-    df1 = df.iloc[:, :split_point]
-    df2 = df.iloc[:, split_point:]
-    return df1, df2
+    # Create the HTML table structure manually
+    html = '<table class="dataframe">'
 
+    # Header row
+    html += '<tr>'
+    html += '<th class="percent-header"></th>'  # Add Percent to the first row with special class
+    for column in df.columns:
+        html += f'<th>{column}</th>'
+    html += '</tr>'
 
-def format_table_for_slack(df):
+    # Data rows
+    for index, row in df.iterrows():
+        html += '<tr>'
+        html += f'<td class="percent-cell">{index}</td>'  # Percent column with special class
+        for value in row:
+            html += f'<td>{value}</td>'
+        html += '</tr>'
+
+    html += '</table>'
+
+    # Define CSS for table styling
+    css = """
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Comfortaa:wght@400;700&display=swap');
+
+        body {
+            margin: 0;
+            padding: 0;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            min-height: 100vh;
+            background-color: #2C2A63;
+        }
+        .dataframe {
+            border-collapse: separate;
+            border-spacing: 0;
+            font-family: 'Comfortaa', sans-serif;
+            background-color: #2C2A63;
+            color: white;
+            border-radius: 20px;
+            overflow: hidden;
+            max-width: 100%;
+            margin: auto;
+        }
+        .dataframe th, .dataframe td {
+            border: 1px solid #ddd;
+            padding: 8px;
+            text-align: left;
+            font-size: 14px;
+            font-weight: 600;
+            max-width: 150px;
+            overflow: hidden;
+            text-overflow: ellipsis;
+        }
+        .dataframe th {
+            background-color: #181A1B;
+            color: white;
+            font-weight: bold;
+            font-size: 16px;
+        }
+        .dataframe .percent-header, .dataframe .percent-cell {
+            background-color: #181A1B;
+            color: white;
+            font-weight: bold;
+            font-size: 16px;
+        }
+        .dataframe tr:first-child th:first-child {
+            border-top-left-radius: 20px;
+        }
+        .dataframe tr:first-child th:last-child {
+            border-top-right-radius: 20px;
+        }
+        .dataframe tr:last-child td:first-child {
+            border-bottom-left-radius: 20px;
+        }
+        .dataframe tr:last-child td:last-child {
+            border-bottom-right-radius: 20px;
+        }
+    </style>
     """
-    Formats the DataFrame into a tabulated string for Slack with dashed lines under headers.
+
+    full_html = f"""
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <meta charset="UTF-8">
+        {css}
+    </head>
+    <body>
+        {html}
+    </body>
+    </html>
     """
-    # Convert DataFrame to string with tabulate using 'plain' format
-    table_str = tabulate(df, headers='keys', tablefmt='simple')
 
-    return table_str
+    return full_html
 
 
-def send_intro_message(channel_id, summary):
+def html_to_image(html_content, output_path):
+    """
+    Convert HTML content to an image file.
+    """
+    options = {
+        "transparent": "",
+        'format': 'png',
+        'encoding': "UTF-8",
+        'width': 1400,
+        'quality': 100,
+    }
+    imgkit.from_string(html_content, output_path, options=options)
+
+
+def send_message_and_table(channel_id, summary, image_path):
+    """
+    Send a message and an image file to Slack.
+    """
     try:
+        # Intro message
         intro_message = (
             f">*Vztah znamení k ostatním znamení ke dni: _{time.strftime('%d.%m.%Y')}_*\n"
             f"{summary}\n"
         )
-        response = client.chat_postMessage(
+        response_message = client.chat_postMessage(
             channel=channel_id,
             text=intro_message
         )
-        print(f"Message sent to {channel_id}: {response['ts']}")
+        print(f"Message sent to {channel_id}: {response_message['ts']}")
+
+        # Image upload
+        response_file = client.files_upload_v2(
+            channels=channel_id,
+            file=image_path,
+            title="Horoscope Compatibility Table",
+        )
+        print(f"Image sent to {channel_id}: {response_file['file']['id']}")
 
     except SlackApiError as e:
-        print(f"Error sending message to Slack: {e.response['error']}")
-
-
-def send_table_to_slack(channel_id, table_str):
-    """
-    Sends a tabulated string to a Slack channel.
-    """
-    try:
-        # Split table into multiple messages if necessary
-        message_chunks = [table_str[i:i + 4000] for i in range(0, len(table_str), 4000)]
-
-        for chunk in message_chunks:
-            response = client.chat_postMessage(
-                channel=channel_id,
-                text=f"\n```\n{chunk}\n```"
-            )
-            print(f"Message sent to {channel_id}: {response['ts']}")
-
-    except SlackApiError as e:
-        print(f"Error sending message to Slack: {e.response['error']}")
+        print(f"Error sending message or image to Slack: {e.response['error']}")
 
 
 def _remove_diacritics(text):
     """
-    Removes diacritics (accents) from the text.
+    Remove diacritics from a given text.
     """
     return ''.join(c for c in unicodedata.normalize('NFD', text)
                    if unicodedata.category(c) != 'Mn')
@@ -159,17 +243,10 @@ def _remove_diacritics(text):
 
 def generate_relationship_summary(df, names_zodiacs, percentage, relationship_type):
     """
-    Generates a summary of relationships based on specified compatibility percentage.
-
-    :param df: DataFrame containing compatibility data
-    :param names_zodiacs: Dictionary mapping zodiac signs to names
-    :param percentage: Compatibility percentage to look for (100 or -100)
-    :param relationship_type: Type of relationship ("kamarád" or "nepřítel")
-    :return: Summary of relationships
+    Generate a summary of relationships based on compatibility percentage.
     """
     relationships_dict = {}
 
-    # Iterate over each zodiac sign and corresponding names
     for zodiac, names in names_zodiacs.items():
         zodiac_no_diacritics = _remove_diacritics(zodiac).capitalize()
         if zodiac_no_diacritics in df.columns:
@@ -185,14 +262,12 @@ def generate_relationship_summary(df, names_zodiacs, percentage, relationship_ty
                                 relationships_dict[name] = set()
                             relationships_dict[name].update(related_names_list)
 
-    # Create a set of unique relationships
     unique_relationships = set()
     for name, related in relationships_dict.items():
         for person in related:
             relationship = tuple(sorted([name, person]))
             unique_relationships.add(relationship)
 
-    # Aggregate relationships
     aggregated_relationships = {}
     for relationship in unique_relationships:
         key = relationship[0]
@@ -200,7 +275,6 @@ def generate_relationship_summary(df, names_zodiacs, percentage, relationship_ty
             aggregated_relationships[key] = set()
         aggregated_relationships[key].add(relationship[1])
 
-    # Create summary lines
     summary_lines = []
     for name, related in aggregated_relationships.items():
         related_string = ', '.join(sorted(related))
@@ -214,35 +288,23 @@ def generate_relationship_summary(df, names_zodiacs, percentage, relationship_ty
 
 def send_daily_horoscope():
     """
-    Fetches and sends the daily horoscope data to the Slack channel.
+    Fetch, format, and send daily horoscope data.
     """
     raw_compatibility_data = fetch_zodiac_data(BASE_URL, ZODIACS)
     formatted_compatibility_data = format_compatibility_data(raw_compatibility_data, PERCENTAGES, NAMES_ZODIACS)
 
-    # Split DataFrame into two parts for better readability
-    df1, df2 = split_dataframe(formatted_compatibility_data)
-
-    # Generate summaries for friends and enemies
     friends_summary = generate_relationship_summary(formatted_compatibility_data, NAMES_ZODIACS, 100, "kamarád")
     enemies_summary = generate_relationship_summary(formatted_compatibility_data, NAMES_ZODIACS, -100, "nepřítel")
 
-    # Combine summaries
     combined_summary = f"Kamarádi:\n{friends_summary}\n\nNepřátelé:\n{enemies_summary}"
 
-    # Send intro message with summaries
-    send_intro_message(SLACK_CHANNEL_ID, combined_summary)
+    html_content = dataframe_to_html(formatted_compatibility_data)
+    html_to_image(html_content, IMG_PATH)
 
-    # Format DataFrame parts into tabulated strings
-    table_str1 = format_table_for_slack(df1)
-    table_str2 = format_table_for_slack(df2)
+    send_message_and_table(SLACK_CHANNEL_ID, combined_summary, IMG_PATH)
 
-    # Send the tabulated strings to Slack
-    send_table_to_slack(SLACK_CHANNEL_ID, table_str1)
-    send_table_to_slack(SLACK_CHANNEL_ID, table_str2)
 
 if __name__ == "__main__":
     send_daily_horoscope()
-
-    # Run the Slack bot using Socket Mode
     handler = SocketModeHandler(app, SLACK_APP_TOKEN)
     handler.connect()
